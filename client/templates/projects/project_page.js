e5e07e73ff2633984db34fc0317fb60a7fd57cea
Frank Niessink <frank@niessink.com>
2015-04-05 20:26:36 +0200

    Only show findings list when the project has sources.

--- client/templates/projects/project_page.js
+++ client/templates/projects/project_page.js
@@ -4,6 +4,8 @@ Template.projectPage.helpers({
   },
   sources: function() {
     return Sources.find({projectId: this._id});
+  },
+  has_sources: function() {
+    return Sources.find({projectId: this._id}).count() > 0;
   }
 });
-
