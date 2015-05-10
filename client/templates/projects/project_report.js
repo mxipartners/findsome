1e91f405aad14096d91ca75bffb0e71f9e45f945
Frank Niessink <frank@niessink.com>
2015-05-10 22:28:36 +0200

    Prevent empty tables in the report.

--- client/templates/projects/project_report.js
+++ client/templates/projects/project_report.js
@@ -2,14 +2,23 @@ Template.projectReport.helpers({
   risks: function() {
     return Risks.find({projectId: this._id});
   },
+  has_risks: function() {
+    return Risks.find({projectId: this._id}).count() > 0;
+  },
   riskFindings: function() {
     return Findings.find({_id: {$in: this.findings}});
   },
   findings: function() {
     return Findings.find({projectId: this._id});
   },
+  has_findings: function() {
+    return Findings.find({projectId: this._id}).count() > 0;
+  },
   sources: function() {
     return Sources.find({projectId: this._id});
+  },
+  has_sources: function() {
+    return Sources.find({projectId: this._id}).count() > 0;
   },
   findingSources: function() {
     return Sources.find({_id: {$in: this.sources}});
