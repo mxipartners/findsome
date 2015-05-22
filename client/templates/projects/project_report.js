ae50e0e7242cff15fef0fe947b7235d8ffce80ac
Frank Niessink <frank@niessink.com>
2015-05-22 13:14:14 +0200

    Reorder report. Closes #22.

--- client/templates/projects/project_report.js
+++ client/templates/projects/project_report.js
@@ -1,13 +1,25 @@
-Template.projectReport.helpers({
-  measures: function() {
-    return Measures.find({projectId: this._id});
+Template.projectSourcesTable.helpers({
+  sources: function() {
+    return Sources.find({projectId: this._id});
   },
-  has_measures: function() {
-    return Measures.find({projectId: this._id}).count() > 0;
+  has_sources: function() {
+    return Sources.find({projectId: this._id}).count() > 0;
+  }
+});
+
+Template.projectFindingsTable.helpers({
+  findings: function() {
+    return Findings.find({projectId: this._id});
   },
-  measureRisks: function() {
-    return Risks.find({_id: {$in: this.risks}});
+  has_findings: function() {
+    return Findings.find({projectId: this._id}).count() > 0;
   },
+  findingSources: function() {
+    return Sources.find({_id: {$in: this.sources}});
+  }
+});
+
+Template.projectRisksTable.helpers({
   risks: function() {
     return Risks.find({projectId: this._id});
   },
@@ -16,20 +28,17 @@ Template.projectReport.helpers({
   },
   riskFindings: function() {
     return Findings.find({_id: {$in: this.findings}});
+  }
+});
+
+Template.projectMeasuresTable.helpers({
+  measures: function() {
+    return Measures.find({projectId: this._id});
   },
-  findings: function() {
-    return Findings.find({projectId: this._id});
+  has_measures: function() {
+    return Measures.find({projectId: this._id}).count() > 0;
   },
-  has_findings: function() {
-    return Findings.find({projectId: this._id}).count() > 0;
-  },
-  sources: function() {
-    return Sources.find({projectId: this._id});
-  },
-  has_sources: function() {
-    return Sources.find({projectId: this._id}).count() > 0;
-  },
-  findingSources: function() {
-    return Sources.find({_id: {$in: this.sources}});
+  measureRisks: function() {
+    return Risks.find({_id: {$in: this.risks}});
   }
 });
