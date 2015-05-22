15b23246dc72d1dfee7b1b4e6a331f1f03d7eb85
Frank Niessink <frank@niessink.com>
2015-05-22 22:43:49 +0200

    Add new users to an example project.

---
+++ server/user_initialization.js
@@ -0,0 +1,22 @@
+Accounts.onCreateUser(function(options, user) {
+  tutorial = {
+    title: "Welcome to Findsome, " + user.username + "!",
+    description: "Findsome aims to help you do investigations, intakes, and " +
+      "due dilligence projects. It allows you to register sources, findings, " +
+      "risks, and mitigating measures. And it helps you to keep track of the " +
+      "relationships between them. This is an example project. Feel free to " +
+      "add some sources, findings, risks, and measures to it and delete the " +
+      "project when you're done with it.",
+    members: [user._id],
+    userId: user._id,
+    author: user.username,
+    submitted: new Date(),
+    measuresCount: 0,
+    risksCount: 0,
+    findingsCount: 0,
+    sourcesCount: 0,
+    kind: 'project'
+  };
+  Projects.insert(tutorial);
+  return user;
+});
