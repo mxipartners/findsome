6de408c73b08e26b89a7a84680a5fa8e085bf437
Frank Niessink <frank@niessink.com>
2015-05-25 14:03:18 +0200

    Add counts of items to tabs. Closes #24.

--- client/templates/projects/project_tabs.js
+++ client/templates/projects/project_tabs.js
@@ -2,14 +2,26 @@ Template.projectTabs.helpers({
   sources: function() {
     return Sources.find({projectId: this._id});
   },
+  sources_count: function() {
+    return Sources.find({projectId: this._id}).count();
+  },
   findings: function() {
     return Findings.find({projectId: this._id});
+  },
+  findings_count: function() {
+    return Findings.find({projectId: this._id}).count();
   },
   risks: function() {
     return Risks.find({projectId: this._id});
   },
+  risks_count: function() {
+    return Risks.find({projectId: this._id}).count();
+  },
   measures: function() {
     return Measures.find({projectId: this._id});
+  },
+  measures_count: function() {
+    return Measures.find({projectId: this._id}).count();
   },
   has_sources: function() {
     return Sources.find({projectId: this._id}).count() > 0;
